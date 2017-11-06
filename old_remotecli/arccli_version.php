8574a45b1a5504ea3380fbbec35041d679039205
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-11-06 15:02:40 +0200

    Refactoring
    Move old application to old_remotecli

---
+++ old_remotecli/arccli_version.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package AkeebaRemote
+ * @copyright Copyright (c)2006-2017 Nicholas K. Dionysopoulos
+ * @license GNU General Public License version 3, or later
+ * @version $Id$
+ */
+
+define('ARCCLI_VERSION', 'rev5548E25');
+define('ARCCLI_DATE', '2017-05-08');
+define('ARCCLI_MINAPI',319);
