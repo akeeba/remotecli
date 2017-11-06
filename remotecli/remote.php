c0a11a957e6b4e151440862baaeec8a527f27b6d
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2017-11-06 21:15:48 +0200

    Refactor
    Add PHAR compatibility in main entry point file

--- remotecli/remote.php
+++ remotecli/remote.php
@@ -1,10 +1,15 @@
-#!/bin/env php
 <?php
 /**
  * @package    AkeebaRemoteCLI
  * @copyright  Copyright (c)2006-2017 Nicholas K. Dionysopoulos
  * @license    GNU General Public License version 3, or later
  */
+
+// Enable compatibility with PHAR archives
+if (Phar::running(false))
+{
+	Phar::interceptFileFuncs();
+}
 
 /** @var Composer\Autoload\ClassLoader $autoloader */
 $autoloader = include_once __DIR__ . '/vendor/autoload.php';
