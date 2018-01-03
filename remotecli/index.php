f5f96cc7161eea40cc36f580beea74f873c8e75b
Nicholas K. Dionysopoulos <nicholas@akeebabackup.com>
2018-01-03 10:49:41 +0200

    Copyright dates update. Happy New Year 2018!

--- remotecli/index.php
+++ remotecli/index.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    AkeebaRemoteCLI
- * @copyright  Copyright (c)2006-2017 Nicholas K. Dionysopoulos
+ * @copyright Copyright (c)2008-2018 Nicholas K. Dionysopoulos / Akeeba Ltd
  * @license    GNU General Public License version 3, or later
  *
  * This file is required for the PHAR archive to be executable. Do not remove.
